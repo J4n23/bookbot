ea24f7329ff032c27462ea69754233d8c49665fa
J4n23 <vondrsen@gmail.com>
2025-04-29 13:55:47 +0200

    C: Separate counter

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import word_counter
+
 
 def get_book_text(path):
     with open (path) as f:
         file_contents = f.read()
         return file_contents
-
-def word_counter(text):
-    words = text.split()
-    return len(words)
-
 
 def main():
     result = get_book_text("books/frankenstein.txt")
